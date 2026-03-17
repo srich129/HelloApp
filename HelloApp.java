5e0b713513cd00c97604bdcea23e4e59834b2a7a
et sree charitha <yourname@example.com>
2026-03-17 21:00:59 +0530

    UC6: Added substring method greeting

--- HelloApp.java
+++ HelloApp.java
@@ -1,5 +1,22 @@
 public class HelloApp {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        // Default message
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+            return;
+        }
+
+        String result = "";
+
+        // Enhanced for loop
+        for (String name : args) {
+            result += name + ", ";
+        }
+
+        // Remove last comma and space
+        result = result.substring(0, result.length() - 2);
+
+        System.out.println("Hello, " + result + "!");
     }
 }
